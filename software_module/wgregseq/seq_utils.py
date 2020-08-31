a746bad35125b42ff00437cd37ab00beae9a9633
scott-saunders <ssaunder@caltech.edu>
2020-08-31 13:07:52 -0700

    update to gen_emat_single_site()

--- software_module/wgregseq/seq_utils.py
+++ software_module/wgregseq/seq_utils.py
@@ -166,7 +166,8 @@ def gen_emat_rand(site_size):
     """
     return(np.random.normal(1,1,(site_size,4)))
 
-
+'''
+# old version
 def gen_emat_single_site(seq, site_start, site_size):
     """
     Generate energy matrix for sequence with one site. Outside of site matrix is zero.
@@ -189,7 +190,54 @@ def gen_emat_single_site(seq, site_start, site_size):
     seq_emat[site_start:(site_start + site_size),:] = gen_emat_rand(site_size)
     
     return(seq_emat)
+'''
 
+def gen_emat_single_site(seq, site_start, site_size, site_mean = 1, site_sd = 1, background_mean = 0, background_sd = 0):
+    """
+    Generate energy matrix for sequence with one site. 
+    Mean and sd values can be set for site and non site positions.
+    WT sequence is set to zero.
+    
+    Parameters
+    ----------
+    seq : string
+    
+    site_start : int
+    
+    site_size : int
+    
+    site_mean: float
+        mean energy for site mutations, for np.random.normal
+        
+    site_sd: float
+        standard deviation of energy for site mutations, for np.random.normal
+    
+    background_mean: float
+        mean energy for non site mutations, for np.random.normal
+    
+    background_sd: float
+        standard deviation of energy for non site mutations, for np.random.normal
+    
+    Returns
+    ---------
+    seq_emat : pd.DataFrame
+        generated energy matrix
+    """
+    
+    # Set background values
+    seq_emat = np.random.normal(background_mean,background_sd,(len(seq),4))
+    
+    # Set site values
+    seq_emat[site_start:(site_start + site_size),:] = np.random.normal(site_mean, site_sd,(site_size,4))
+    
+    # Convert np.array to pd.DataFrame
+    seq_emat = pd.DataFrame(data = seq_emat, columns = ('A','T','C','G'))
+    
+    # Set WT values = 0
+    for ind,char in enumerate(seq, start = 0):
+        seq_emat.iloc[ind][char]=0
+    
+    return(seq_emat)
 
 def sum_emat(seq, emat):
     """
